7e01b0b63dedb393eafba63753554a3032664780
latreta <yves_henry13@hotmail.com>
2019-07-25 00:02:29 -0300

    sidemenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,11 @@ import { SignInComponent } from './home/component/signin/signin.component';
 import { CadastraBlocoComponent } from './bloco/components/cadastra-bloco/cadastra-bloco.component';
 import { CadastraSalaComponent } from './sala/component/cadastra-sala/cadastra-sala.component';
 import { CadastraDicenteComponent } from './dicente/components/cadastra-dicente/cadastra-dicente.component';
+import { MinhaContaComponent } from './user/components/minha-conta/minha-conta.component';
 
 const routes: Routes = [
   {path: '', component: ListarAulaComponent},
+  {path: 'conta', component: MinhaContaComponent},
   {path: 'salas/adicionar', component: CadastraSalaComponent},
   {path: 'aulas/adicionar', component: CadastraAulaComponent},
   {path: 'dicentes/adicionar', component: CadastraDicenteComponent},
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CadastraUsuarioComponent } from './components/cadastra.usuario/cadastra.usuario.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MinhaContaComponent } from './components/minha-conta/minha-conta.component';
 
 
 @NgModule({
-    declarations:[CadastraUsuarioComponent],
+    declarations:[CadastraUsuarioComponent, MinhaContaComponent],
     imports: [
         FormsModule,
         ReactiveFormsModule,
