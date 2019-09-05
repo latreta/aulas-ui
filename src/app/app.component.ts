eccb50fb4ff0373d7145e308ddfef4f52264e146
latreta <yves_henry13@hotmail.com>
2019-09-05 18:42:00 -0300

    misc things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
 export class AppComponent implements OnInit{
-  title = 'policlass-ui';
+  title = 'PoliClass';
 
 
   ngOnInit() {
