dcd995c1b81b33bc910d3882b5585eea315dd49d
latreta <yveshenr@gmail.com>
2020-05-17 03:36:10 -0300

    ngrx trying out

---
+++ src/app/store/entity-metadata.ts
@@ -0,0 +1,13 @@
+import { EntityMetadataMap } from '@ngrx/data';
+
+const entityMetadata: EntityMetadataMap = {
+    Aula: {},
+    Sala: {},
+    Discente: {},
+    Disciplina: {},
+    Bloco: {},    
+};
+
+export const entityConfig = {
+    entityMetadata
+};
